159824a3b76363795faecb2e354301e67fcae4c2
Jennifer Meier <meierjl01@gmail.com>
2016-10-16 16:56:19 -0500

    working on second view

--- images.js
+++ images.js
@@ -1,79 +1,103 @@
 var albums = [{
-    'albumName': 'album1',
+    'albumName': 'One',
     'albumPreview': 'images/1.jpg',
+    'hash': '#albumOne',
     'images': [{
         'url': 'images/1.jpg',
         'imageName': 'img1',
+        'hash': '#pictureOne',
     }, {
         'url': 'images/2.jpg',
         'imageName': 'img2',
+        'hash': '#pictureTwo',
     }, {
         'url': 'images/3.jpeg',
         'imageName': 'img3',
+        'hash': '#pictureThree',
     }]
 }, {
-    'albumName': 'album2',
+    'albumName': 'Two',
     'albumPreview': 'images/img4.jpeg',
+    'hash': '#albumTwo',
     'images': [{
         'url': 'images/4.jpeg',
         'imageName': 'img4',
+        'hash': '#pictureFour',
     }, {
         'url': 'images/5.jpeg',
         'imageName': 'img5',
+        'hash': '#pictureFive',
     }, {
         'url': 'images/6.jpeg',
         'imageName': 'img6',
+        'hash': '#pictureSix',
     }]
 }, {
-    'albumName': 'album3',
+    'albumName': 'Three',
     'albumPreview': 'images/img7.jpeg',
+    'hash': '#albumThree',
     'images': [{
         'url': 'images/7.jpeg',
         'imageName': 'img7',
+        'hash': '#pictureSeven',
     }, {
         'url': 'images/8.jpeg',
         'imageName': 'img8',
+        'hash': '#pictureEight',
     }, {
         'url': 'images/9.jpeg',
         'imageName': 'img9',
+        'hash': '#pictureNine',
     }]
 }, {
-    'albumName': 'album4',
+    'albumName': 'Four',
     'albumPreview': 'images/img10.jpeg',
+    'hash': '#albumFour',
     'images': [{
         'url': 'images/10.jpeg',
         'imageName': 'img10',
+        'hash': '#pictureTen',
     }, {
         'url': 'images/11.jpeg',
         'imageName': 'img11',
+        'hash': '#pictureEleven',
     }, {
         'url': 'images/12.jpeg',
         'imageName': 'img12',
+        'hash': '#pictureTwelve',
     }]
 }, {
-    'albumName': 'album5',
+    'albumName': 'Five',
     'albumPreview': 'images/img13.jpeg',
+    'hash': '#albumFive',
     'images': [{
         'url': 'images/13.jpeg',
         'imageName': 'img13',
+        'hash': '#pictureThirteen',
     }, {
         'url': 'images/14.jpeg',
         'imageName': 'img14',
+        'hash': '#pictureFourteen',
     }, {
         'url': 'images/15.jpeg',
         'imageName': 'img15',
+        'hash': '#pictureFifteen',
     }]
 }, {
-    'albumName': 'album6',
+    'albumName': 'Six',
     'albumPreview': 'images/img16.jpeg',
+    'hash': '#albumSix',
     'images': [{
         'url': 'images/16.jpeg',
         'imageName': 'img16',
+        'hash': '#pictureSixteen',
     }, {
         'url': 'images/17.jpeg',
         'imageName': 'img17',
+        'hash': '#pictureSeventeen',
     }, {
         'url': 'images/18.jpeg',
         'imageName': 'img18',
+        'hash': '#pictureEighteen',
     }]
 }];
--- main.js
+++ main.js
@@ -1,10 +1,75 @@
-console.log(albums[0].images[0].url);
+// console.log(albums[0].images[0].url);
 var sideLinks = $('.sideLinks');
 var header = $('.page-header');
 var content = $('.content');
 
 
 //testing to get album 1 preview on the page:
-var album1 = $('<div><img class="pic" src='+albums[0].images[0].url+'><span>Album 1</span></div>');
-console.log(album1);
-content.append(album1);
+// var album1 = $('<div><img class="pic" src='+albums[0].images[0].url+'><span>Album 1</span></div>');
+// console.log(album1);
+// content.append(album1);
+
+
+//For Each: need to take each album and also include, the hash for location, their Preview image, and their name in a span.
+//Need to append that to the content container, like in the test above
+albums.forEach(function(album, i, arr) {
+  var albumPreviewItem = $('<a class="previewItem" href="' + album.hash + '"><img src = "' + album.images[0].url + '" /img><h2>' + album.albumName + '</h2></a>');
+  content.append(albumPreviewItem);
+});
+
+
+// when an album is clicked, show a new page with all of the photos in that album listed out, under the album name. To the left, show the nav bar on the left so user can click on any of the albums to navigate around
+
+//on a click event, for each loop for album pictures to be displayed:
+
+function pickAlbum (album, i, arr) {
+  return album.albumName === 'One';
+}
+
+function albumPics (album, i, arr) {
+  return album.images;
+}
+  var chooseAlbum = albums.filter(pickAlbum);
+  console.log(chooseAlbum);
+
+  var albumImages = albums.map(albumPics);
+  console.log(albumImages);
+
+function pickImage (header, content) {
+  console.log('hi');
+}
+
+pickImage(header,content);
+
+var previewItem = $('.previewItem');
+previewItem.on('click', function(e) {
+    console.log('hi');
+    content.empty();
+
+albums.forEach(function(album, i, arr) {
+      var albumItems = $('<a class="albumItems" href="' + album.images.hash + '"><img src = "' + album.images[i].url + '" /img><h2>' + album.albumName + '</h2></a>');
+      content.append(albumItems);
+    });
+
+});
+
+
+
+
+// var album1 = albums.filter(function(album, i, arr) {
+//   if (album.albumName === 'One') {
+//     return true;
+//   }
+// });
+// console.log(album1);
+//
+// var album1Images = album1.filter(function(image, i, arr) {
+//   if (image.)
+// });
+
+
+
+    // albums.forEach(function(album, i, arr) {
+    //   var albumpicItem = $('<a href="' + album[0].images.hash +'">< img src = "' + album[0].images[i].url + '" /img><h2>' + album[0].albumName + '</h2></a>');
+    //   content.append(albumpicItem);
+    // });
